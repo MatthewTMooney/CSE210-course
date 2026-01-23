e9cdee78f99eb0e18867643f21eb2e81816f7fa6
MatthewTMooney <mmooney@byupathway.edu>
2026-01-23 19:51:48 +0000

    push4

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+
+class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+    private Random _random = new Random();
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+
+        string[] parts = text.Split(" ");
+        foreach (string part in parts)
+        {
+            _words.Add(new Word(part));
+        }
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        for (int i = 0; i < numberToHide; i++)
+        {
+            int index = _random.Next(_words.Count);
+            _words[index].Hide();
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string text = "";
+
+        foreach (Word word in _words)
+        {
+            text += word.GetDisplayText() + " ";
+        }
+
+        return $"{_reference.GetDisplayText()}\n{text.Trim()}";
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
